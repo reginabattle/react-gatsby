61d97ebd11964a893514b764fc0658cda628a262
Regina Battle <hello@reginabattle.com>
2019-03-05 13:18:28 -0500

    Add build folder

---
+++ public/path----87aeb1b39632d91c12ea.js
@@ -0,0 +1,2 @@
+webpackJsonp([60335399758886],{123:function(t,e){t.exports={data:{site:{siteMetadata:{title:"Design + Code"}}},layoutContext:{}}}});
+//# sourceMappingURL=path----87aeb1b39632d91c12ea.js.map
